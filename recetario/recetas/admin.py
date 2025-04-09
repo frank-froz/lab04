845fc4bac1bad05f28702ed1b39021011dcfc0e9
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-09 16:00:05 -0500

    creación de las vistas receta y detalles receta

--- recetario/recetas/admin.py
+++ recetario/recetas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Receta, Categoria
 
-# Register your models here.
+admin.site.register(Receta)
+admin.site.register(Categoria)
--- recetario/recetas/views.py
+++ recetario/recetas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Receta
 
-# Create your views here.
+
+def lista_recetas(request):
+    recetas = Receta.objects.all().order_by('-fecha_creacion')
+    return render(request, 'recetas/lista_recetas.html', {'recetas': recetas})
+
+
+def detalle_receta(request, receta_id):
+    receta = get_object_or_404(Receta, id=receta_id)
+    return render(request, 'recetas/detalle_receta.html', {'receta': receta})
